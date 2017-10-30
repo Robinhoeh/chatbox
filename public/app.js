0d533a0686087628ce005df2c3cb588d243be4d0
Robin Watson <drumsrobin@gmail.com>
2017-10-29 21:29:39 -0400

    GET / error

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
+ var socket = io();//reference socket library
+
 $('form').on('submit', function() {//on click of submit button
-	var text = $('#message').val();//get value of input
-	alert(text);// alert input message
+	socket.emit('message', text);//sends the message to the server
+	$('#message').val('');//clears the input once submitted - SETS empty
 	return false;//cancel form submission 
+});
+
+socket.on('message', function (){//when a message received from real time web sockets
+	$('<li>').text(msg).appendTo('#history');//create new li, append msg to the <ol>
 });
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 console.log("hello from server.js");
+
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client') );
+
+//socket.io
+var io = require('socket.io')(server);
+
+io.on('connection', function() {
+	socket.io('message', function (msg) {
+	io.emit('message', msg);
+	})
+});
+//socket.io
+
+
+server.listen(8080, function(){//tell server to listen to port 8080
+	console.log('Chat server running...');
+});
